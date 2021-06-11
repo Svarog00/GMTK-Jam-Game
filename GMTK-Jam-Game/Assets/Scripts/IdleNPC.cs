deaaa307a31fc3d3a94671cda7a8236906db23c1
Anton Verkhovets <54817442+Svarog00@users.noreply.github.com>
2021-06-11 19:30:25 +0300

    Init project

---
+++ GMTK-Jam-Game/Assets/Scripts/IdleNPC.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdleNPC : MonoBehaviour
+{
+    public DialogSystemNew dialog;
+
+    private TextScript _note;
+    private PlayerControl playerControl;
+
+    private bool _isNearby;
+
+    private void Start()
+    {
+        _isNearby = false;
+        _note = FindObjectOfType<TextScript>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && _isNearby)
+        {
+            playerControl.CanAttack = false;
+            dialog.showDialog = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            _note.Appear("Press E to talk.", 2f);
+            _isNearby = true;
+
+            if (playerControl == null) playerControl = collision.GetComponent<PlayerControl>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            dialog.showDialog = false;
+            playerControl.CanAttack = true;
+            _note.Disappear(2f);
+            _isNearby = false;
+        }
+    }
+
+    public void Hurt(int damage)
+    {
+        
+    }
+}
---
+++ GMTK-Jam-Game/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //public WeaponScript weapon;
+
+    public bool CanAttack { get; set; }
+    public bool CanMove { get; set; }
+
+    //private Player_Attack player_Attack;
+    private PlayerMovement playerMovement;
+    //private Player_Healing player_Healing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        //player_Healing = GetComponent<Player_Healing>();
+        //player_Attack = GetComponent<Player_Attack>();
+        CanAttack = true;
+        CanMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInput();
+        //HealingInput();
+        //MeleeAttackInput();
+        //GunAttackInput();
+    }
+
+    void MovementInput()
+    {
+        if (CanMove)
+        {
+            playerMovement.HandleMove(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetButtonDown("Dash"));
+        }
+    }
+
+    /*void HealingInput()
+    {
+        if (Input.GetButtonDown("Heal"))
+        {
+            player_Healing.HandleHeal();
+        }
+    }
+
+    void MeleeAttackInput()
+    {
+        if (Input.GetButtonDown("MeleeStrike") && CanAttack)
+        {
+            player_Attack.Melee();
+        }
+    }
+
+    void GunAttackInput()
+    {
+        if (Input.GetButtonDown("GunFire") && CanAttack)
+        {
+            weapon.Attack();
+        }
+    }*/
+}
