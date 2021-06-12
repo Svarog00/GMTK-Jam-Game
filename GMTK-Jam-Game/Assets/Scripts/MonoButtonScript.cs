0f6bb02a350141832a0bf471afc4696a3e7871c1
Anton Verkhovets <54817442+Svarog00@users.noreply.github.com>
2021-06-12 16:28:04 +0300

    Button anim

--- GMTK-Jam-Game/Assets/Scripts/MonoButtonScript.cs
+++ GMTK-Jam-Game/Assets/Scripts/MonoButtonScript.cs
@@ -13,16 +13,25 @@ public class MonoButtonScript : MonoBehaviour
         public SlimeType senderType;
     }
     private SlimeType _buttonType;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("BlueSlime"))
         {
+            _animator.SetTrigger("BlueStand");
             _buttonType = SlimeType.Blue;
             OnButtonActivated?.Invoke(this, new OnButtonActivatedEventArgs { senderType = _buttonType });
         }
         else if (collision.CompareTag("RedSlime"))
         {
+            _animator.SetTrigger("RedStand");
             _buttonType = SlimeType.Red;
             OnButtonActivated?.Invoke(this, new OnButtonActivatedEventArgs { senderType = _buttonType });
         }
