53f29140b6f33b164ab46bc0303f2adce69700df
Anton Verkhovets <54817442+Svarog00@users.noreply.github.com>
2021-06-12 16:51:48 +0300

    First level done

--- GMTK-Jam-Game/Assets/Scripts/DoorScript.cs
+++ GMTK-Jam-Game/Assets/Scripts/DoorScript.cs
@@ -7,12 +7,14 @@ public class DoorScript : MonoBehaviour
     [SerializeField] private MonoButtonScript _activatorButton;
     [SerializeField] private SlimeType _activatorButtonType;
     private BoxCollider2D _boxCollider;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _activatorButton.OnButtonActivated += ActivatorButton_OnButtonActivated;
         _boxCollider = GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void ActivatorButton_OnButtonActivated(object sender, MonoButtonScript.OnButtonActivatedEventArgs e)
@@ -27,6 +29,7 @@ public class DoorScript : MonoBehaviour
     private void Open()
     {
         _boxCollider.isTrigger = true;
+        _animator.SetTrigger("Opened");
         Invoke("SetActiveFalse", 0.5f);
     }
 
